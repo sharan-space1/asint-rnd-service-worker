dfbf2dbbcde53e4ac86fe064cc558483eb41f356
Sharan N <sharan.n@asint.net>
2023-09-13 17:24:13 +0000

    Init: MTA with a Managed Approuter and  UI Module

---
+++ module1/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.asint.rnd.sw.module1.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ module1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/asint/rnd/sw/module1/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
